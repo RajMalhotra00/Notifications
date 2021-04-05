966ce4c48f524d90bcec3521aa2cd84d7fd0c02f
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-04-05 14:18:09 +0200

    Initial Commit

---
+++ Notifications/NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsApp.swift
+//  Notifications
+//
+//  Created by Raj Malhotra on 05.04.21.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
